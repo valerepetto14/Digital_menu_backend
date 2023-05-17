4bbe126a22abbf427af8b0afac0ddf390fced2af
Valentin Repetto <valentinrepetto12345@gmail.com>
2023-05-17 00:27:06 -0300

    add re try to connect to db when failed the connection

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import path from "path";
 import sequelize from "./database/connection";
 import cookieParser from "cookie-parser";
 
-
 //import models of database from "./models";
 import "./models/relationships"
 //import routes
@@ -44,13 +43,46 @@ app.use("/subcategories", subCategoriesRoute);
 //error handler, this must be the last middleware, if not, it will not work, because it will not be able to catch the errors
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    try {
-        console.log("Server started on port" + process.env.PORT);
-        sequelize.sync().then(() => {
-            console.log("base de datos creada");
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
+const MAX_RETRIES = 3; // Número máximo de intentos de conexión
+let currentRetry = 0; // Contador de intentos actual
+
+const connectToDatabase = () => {
+  return new Promise((resolve, reject) => {
+    sequelize
+      .sync({ force: true })
+      .then(() => {
+        console.log("Base de datos creada");
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+const startServer = () => {
+  app.listen(3000, () => {
+    console.log("Servidor iniciado en el puerto 3000");
+  });
+}
+
+const initializeApp = () => {
+  connectToDatabase()
+    .then(() => {
+      startServer();
+    })
+    .catch((error) => {
+      console.log(`Error al conectar a la base de datos: ${error.message}`);
+      if (currentRetry < MAX_RETRIES) {
+        currentRetry++;
+        console.log(`Reintentando conexión (${currentRetry}/${MAX_RETRIES})...`);
+        setTimeout(()=>{
+            initializeApp()
+        }, 5000)
+      } else {
+        console.log("Se alcanzó el número máximo de intentos. La aplicación no puede iniciar.");
+      }
+    });
+}
+
+initializeApp();
