85f7fd7e493b60a2937c6e108f4e5664ebc0e71d
Valentin <admin@MacBook-Pro-de-Valentin.local>
2023-06-28 18:27:18 -0300

    fix/authentication

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,25 +1,22 @@
-import { User } from "../models/user";
-import { Request, Response, NextFunction } from "express";
-import { compare, hash } from "bcrypt";
-import uuid4 from "uuid4";
-import jwt from "jsonwebtoken"
-import { checkIfUserExists } from "../utils/checks";
-import { INCORRECT_CREDENTIALS, USER_ALREADY_EXISTS } from "../utils/errors";
-import { extend } from "joi";
-
+import { User } from '../models/user';
+import { Request, Response, NextFunction } from 'express';
+import { compare, hash } from 'bcrypt';
+import uuid4 from 'uuid4';
+import jwt from 'jsonwebtoken';
+import { checkIfUserExists } from '../utils/checks';
+import { INCORRECT_CREDENTIALS, USER_ALREADY_EXISTS } from '../utils/errors';
+import { extend } from 'joi';
 
 export interface AuthRequest extends Request {
     user?: any;
     token?: string;
 }
 
-
-export const signUp = async (req:Request, res:Response, next:NextFunction) => {
+export const signUp = async (req: Request, res: Response, next: NextFunction) => {
     try {
-    
         const body = req.body;
         const userExists = await checkIfUserExists(body.email);
-        if(!userExists){
+        if (!userExists) {
             const passHash = await hash(body.password, 10);
             const newUser = await User.create({
                 id: uuid4(),
@@ -28,60 +25,63 @@ export const signUp = async (req:Request, res:Response, next:NextFunction) => {
                 lastName: body.lastName,
                 phoneNumber: body.phoneNumber,
                 type: body.type,
-                password: passHash
-            })
-            return res.status(201).json({message: "User created", user: newUser});
+                password: passHash,
+            });
+            return res.status(201).json({ message: 'User created', user: newUser });
         } else {
             throw USER_ALREADY_EXISTS;
         }
     } catch (error) {
         return next(error);
     }
-}
+};
 
-export const signIn = async (req:Request, res:Response, next:NextFunction) => {
+export const signIn = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        console.log('start signin');
         const body = req.body;
-        const user = await User.findOne({where: {email: body.email}});
-        if(user){
+        const user = await User.findOne({ where: { email: body.email } });
+        if (user) {
             const passMatch = await compare(body.password, user.password);
-            if(passMatch){
-                const token = jwt.sign({
-                    email: user.email,
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    type: user.type,
-                }, process.env.TOKEN_SIGN ? process.env.TOKEN_SIGN : "HOLA" )
-                console.log(process.env.TOKEN_SIGN);
-                
+            if (passMatch) {
+                const token = jwt.sign(
+                    {
+                        email: user.email,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        type: user.type,
+                    },
+                    process.env.TOKEN_SIGN ? process.env.TOKEN_SIGN : 'HOLA'
+                );
                 const response = {
-                    userId : user.id,
+                    userId: user.id,
                     email: user.email,
                     firstName: user.firstName,
                     lastName: user.lastName,
                     type: user.type,
-                    token: token
+                    token: token,
                 };
-                return res.status(200).cookie("token", token, {httpOnly: true}).json({message: "User logged in", user: response});
+                return res
+                    .status(200)
+                    .cookie('token', token, { httpOnly: true })
+                    .json({ message: 'User logged in', user: response });
             } else {
-                throw INCORRECT_CREDENTIALS
+                throw INCORRECT_CREDENTIALS;
             }
         } else {
-            throw INCORRECT_CREDENTIALS
+            throw INCORRECT_CREDENTIALS;
         }
     } catch (error) {
         return next(error);
     }
-}
+};
 
-export const signOut = async (req:AuthRequest, res:Response, next:NextFunction) => {
+export const signOut = async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
         const user = req.user;
-        if(user){
-            res.clearCookie("token").status(200).json({message: "User logged out"});
+        if (user) {
+            res.clearCookie('token').status(200).json({ message: 'User logged out' });
         }
     } catch (error) {
         next(error);
     }
-}
+};
--- src/middlewares/auth.middlware.ts
+++ src/middlewares/auth.middlware.ts
@@ -1,32 +1,33 @@
-import { Request, NextFunction, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { User } from '../models/user';
 import { UNAUTHORIZED } from '../utils/errors';
 import { AuthRequest } from "../controllers/auth";
+import { UserTypes } from '../utils/types/interfaces';
 
 export const isAuthenticated = async (req:AuthRequest, res: Response, next: NextFunction) => {
     try {
-        console.log('start middleware isAuthenticated', req.url)
-        if(req.url === '/auth/signin' || req.url === '/auth/signup'){
-            return next();
-        }
         const token = req.cookies.token;
         if(!token){
             throw UNAUTHORIZED;
         }
         const payload = verify(token, process.env.TOKEN_SIGN as string) as any;
 
-        console.log('token', token);
         const user = await User.findOne({
             where: {
                 email: payload.email
             }
         })
-        if(user){
-            req.user = user;
-            return next();
+        if (!user)
+            throw UNAUTHORIZED;
+
+        //check type of user
+        if (user.type === UserTypes.ADMIN) {
+            res.locals.userAdmin = user; 
+        } else {
+            res.locals.userEmployee = user;
         }
-        throw UNAUTHORIZED;
+        next();
     } catch (error) {
         next(error);
     }
@@ -34,13 +35,10 @@ export const isAuthenticated = async (req:AuthRequest, res: Response, next: Next
 
 export const isAdmin = async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
-        const user = req.user;
-        if (user){
-            if(user.type === 'admin'){
-                next();
-            }
-        }
-        throw UNAUTHORIZED;
+        const userAdmin = res.locals.userAdmin;
+        if (!userAdmin)
+            throw UNAUTHORIZED;
+        next();
     } catch (error) {
         next(error);
     }
